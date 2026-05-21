56a04eaf79e565f6cd7cd97e0dd55699d1127887
Rohith7276 <140833328+Rohith7276@users.noreply.github.com>
2026-05-21 21:58:53 +0530

    cors

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,16 +6,16 @@ const app = express();
 const server = http.createServer(app);
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    // origin: "https://rapid-chat-five.vercel.app",
+    // origin: process.env.CORS_ORIGIN,
+    origin: "https://rapid-chat-five.vercel.app",
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
 );
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CORS_ORIGIN],
-    // origin: ["https://rapid-chat-five.vercel.app"],
+    // origin: [process.env.CORS_ORIGIN],
+    origin: ["https://rapid-chat-five.vercel.app"],
   },
 });
 const rooms = {}; // add this at top
